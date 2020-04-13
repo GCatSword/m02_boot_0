b0561c3b27c73fe3bdbb14f1deaf9f436e4638bc
GCatSword <61329887+GCatSword@users.noreply.github.com>
2020-04-13 19:41:12 +0200

    funciones classicas

--- p1.py
+++ p1.py
@@ -5,3 +5,17 @@ def sumaTodos(limitTo):
     return resultado
 
 print(sumaTodos(100))
+
+
+
+def sumaTodosCuadrados(limitTo):
+    resultado = 0
+    
+    for i in range(0, limitTo+1):
+        resultado +=i*1
+        
+    return resultado
+
+
+print(sumaTodos(100))
+print(sumaTodosCuadrados(3))
