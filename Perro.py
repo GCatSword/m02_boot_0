6f2a0901a48dc2153adbacf456d0a235ed2e9cb5
GCatSword <61329887+GCatSword@users.noreply.github.com>
2020-04-14 20:24:06 +0200

    Primera version de perro

---
+++ Perro.py
@@ -0,0 +1,18 @@
+class Perro():
+    def __init__(self, nombre, edad, peso):        
+        self.nombre = nombre
+        self.edad = edad
+        self.peso = peso
+
+
+
+
+
+
+
+
+
+
+
+
+
--- objetos.py
+++ objetos.py
@@ -2,11 +2,16 @@ import turtle
 
 t = turtle.Turtle()
 ot = turtle.Turtle()
+l = turtle.Turtle()
 
+l.speed(1)
 
+t.speed(5)
 t.shape('turtle')
+t.color('blue')
 t.fd(50)
 
+ot.speed(5)
 ot.color('green')
 ot.left(90)
 ot.fd(50)
