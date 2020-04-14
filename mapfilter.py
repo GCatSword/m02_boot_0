ba31466a742f288281d278ccc54f994db17e8cf3
GCatSword <61329887+GCatSword@users.noreply.github.com>
2020-04-14 18:53:51 +0200

    map filter reduce

--- mapfilter.py
+++ mapfilter.py
@@ -1,10 +1,34 @@
+from functools import reduce
+
 lista = [1,3,-1,15,9]
 
-listaDobles = map(lambda x: x*2, lista)
 
-listaPares= filter(lambda x: x % 2 == 0, lista)
+def doble(x):
+    return x+x
+
+
+listaDobles = map(lambda x: x*2, lista)
+listaDobles1 = map(doble, lista)
 
 
 print(list(listaDobles))
+print(list(listaDobles1))
+
+
+def esPar(x):
+    return x % 2 == 0
+
+
+listaPares= filter(lambda x: x % 2 == 0, lista)
+listaPares1= filter(esPar, lista)
+
 
 print(list(listaPares))
+print(list(listaPares1))
+
+
+sumatorio = reduce(lambda x, y: x + y, lista)
+suma100 = reduce(lambda x, y: x + y, range(101))
+
+print(sumatorio)
+print(suma100)
